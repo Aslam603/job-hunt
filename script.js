04e7b5f626858cbeca2ba986ec8e4acece35a96c
Shelby1920 <aslamanik28@gmail.com>
2026-02-24 08:27:57 +0600

    made all the eight cards and used togglestyle in buttons

---
+++ script.js
@@ -0,0 +1,45 @@
+let interestList = [];
+let rejectList = [];
+
+
+let total =document.getElementById('total')
+let interestCount = document.getElementById('interestedCount')
+let rejectCount = document.getElementById('rejectedCount')
+
+// console.log(total);
+
+const allCardSection = document.getElementById('allCards')
+
+const mainContainer =document.querySelector('main')
+// console.log(mainContainer)
+
+const allFilterBtn = document.getElementById('all-filter-btn')
+const interestedFilterBtn = document.getElementById('interested-filter-btn')
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn')
+
+
+function calculateCount(){
+    total.innerText =allCardSection.children.length
+    interestCount.innerText = interestList.length
+    rejectCount.innerText = rejectList.length
+}
+calculateCount()
+
+function toggleStyle(id){
+    // console.log("click",id);
+    allFilterBtn.classList.remove('bg-blue-400','text-white')
+    interestedFilterBtn.classList.remove('bg-blue-400','text-white')
+    rejectedFilterBtn.classList.remove('bg-blue-400','text-white')
+
+
+    allFilterBtn.classList.add('bg-amber-50','text-blue-600/60')
+    interestedFilterBtn.classList.add('bg-amber-50','text-blue/600/60')
+    rejectedFilterBtn.classList.add('bg-amber-50','text-blue-600/60')
+
+console.log(id);
+    const selected =document.getElementById(id)
+console.log(selected);
+
+selected.classList.remove('bg-amber-50','text-blue-600/60')
+selected.classList.add('bg-blue-400', 'text-white')
+}
